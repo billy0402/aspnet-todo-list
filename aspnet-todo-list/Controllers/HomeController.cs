4355b2b8187eefff2fe959f93dae14beb8a1d6d2
Billy Huang <10646003@ntub.edu.tw>
2020-12-13 18:49:36 +0800

    feat(home): add index page

---
+++ aspnet-todo-list/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using aspnet_todo_list.Models;
+
+namespace aspnet_todo_list.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbToDoEntities db = new dbToDoEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var todos = db.tToDo.OrderByDescending(m => m.fDate).ToList();
+
+            return View(todos);
+        }
+    }
+}
